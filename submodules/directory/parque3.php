6c77c43f6e8414dc619738de2622ed9b92f416b8
xMeiker <89752513+xMeiker@users.noreply.github.com>
2024-04-28 15:31:59 -0700

    ejemplos detalles

---
+++ submodules/directory/parque3.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="es">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Parque Morelos | Alamo Web</title>
+
+      <!-- enlace css deslizador  -->
+      <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+      <!-- fuente impresionante enlace cdn  -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+      <!-- enlace de archivo css personalizado  -->
+      <link rel="stylesheet" href="/css/details.css">
+
+      <!-- visualizar favicon para la pagina web desde distintos dispositivos -->
+      <link rel="apple-touch-icon" sizes="57x57" href="/images/apple-icon-57x57.png">
+      <link rel="apple-touch-icon" sizes="60x60" href="/images/apple-icon-60x60.png">
+      <link rel="apple-touch-icon" sizes="72x72" href="/images/apple-icon-72x72.png">
+      <link rel="apple-touch-icon" sizes="76x76" href="/images/apple-icon-76x76.png">
+      <link rel="apple-touch-icon" sizes="114x114" href="/images/apple-icon-114x114.png">
+      <link rel="apple-touch-icon" sizes="120x120" href="/images/apple-icon-120x120.png">
+      <link rel="apple-touch-icon" sizes="144x144" href="/images/apple-icon-144x144.png">
+      <link rel="apple-touch-icon" sizes="152x152" href="/images/apple-icon-152x152.png">
+      <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-icon-180x180.png">
+      <link rel="icon" type="image/png" sizes="192x192"  href="/images/android-icon-192x192.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon-96x96.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+      <link rel="manifest" href="/manifest.json">
+      <meta name="msapplication-TileColor" content="#ffffff">
+      <meta name="msapplication-TileImage" content="/images/ms-icon-144x144.png">
+      <meta name="theme-color" content="#ffffff">
+   </head>
+   <body>
+      
+      <!-- comienza la sección del encabezado  -->
+
+      <section class="header">
+
+         <!-- Enlace con la imagen PNG como logo -->
+         <a href="/home.php" class="logo">
+            <!-- Imagen PNG -->
+            <img src="/images/alamoicon.png" alt="AlamoIcon">
+         </a>
+
+         <div class="menu">
+            <img src="/images/alamomenu.png" alt="AlamoMenu">
+            <div class="menu-content">
+                  <a href="/home.php" class="menu-item">Inicio</a>
+                  <a href="/directory.php" class="menu-item">Directorio</a>
+                  <a href="/activities.php" class="menu-item">Actividades</a>
+                  <a href="/blog.php" class="menu-item">Blog de Viajes</a>
+            </div>
+         </div>
+         <div id="menu-btn" class="fas fa-bars"></div>
+
+      </section>
+
+      <!-- termina la sección del encabezado -->
+
+      <!-- inicio sobre la sección de información  -->
+      <div class="heading" style="background:url(/images/header-bg-2.png) no-repeat">
+            <h1>Alamo Web<br> Descubre Lugares en Tijuana</h1>
+      </div>
+
+      <h1 class="heading-title"> Información </h1>
+
+      <section class="container">
+
+         <div class="image2">
+            <img src="/images/about-img.jpg" alt="">
+         </div>
+
+         <div class="content">
+            <h3>Parque Morelos</h3>
+            <p>El Parque Morelos es un lugar multifuncional ubicado en Tijuana, Baja California. Se encuentra al este de la ciudad y ofrece varias actividades y 
+                atracciones para disfrutar. El parque cuenta con un zoológico pequeño, un tren que da un pequeño recorrido, un lago artificial y una zona de juegos. 
+                También hay un jardín botánico y se puede encontrar comida y bicicletas para alquilar. Según los usuarios, el parque es un lugar agradable para relajarse y 
+                estar en contacto con la naturaleza. Sin embargo, también se han mencionado problemas con la calidad de las instalaciones y los animales del zoológico. 
+                En general, el parque tiene una calificación de 4.0 sobre 5.0 con 33 reseñas en Tripadvisor. En resumen, el Parque Morelos es un lugar interesante para visitar 
+                en Tijuana, pero es importante tener en cuenta las críticas y reseñas de otros usuarios antes de planificar una visita.</p>
+            <!--<a href="about.php" class="btn">Leer más</a>-->
+            <a href="about.php" class="btn">Comentarios</a>
+            <a href="https://maps.app.goo.gl/CjQajpbQaG6z5TEu7" class="btn">Ubicación</a>
+         </div>
+      </section>
+
+      <h1 class="heading-title"> <br>Actividades </h1>
+
+      <section class="home">
+
+        <div class="image">
+            <img src="/images/about-img.jpg" alt="">
+         </div>
+
+         <div class="content">
+            <h3>Valoración</h3>
+            <!--<a href="about.php" class="btn">Leer más</a>-->
+            <img src="/images/star2.webp" alt="Star">
+            <img src="/images/star2.webp" alt="Star">
+            <img src="/images/star2.webp" alt="Star">
+            <img src="/images/star1.webp" alt="Star">
+            <img src="/images/star1.webp" alt="Star">
+            <h3>Accesibilidad</h3>
+            <p>• Entrada accesible para personas en silla de ruedas <br> 
+            • Estacionamiento accesible para personas en silla de ruedas</p>
+            <h3>Actividades</h3>
+            <p>• Senderismo <br> 
+            • Ciclismo</p>
+            <h3>Servicios</h3>
+            <p>• Área para patinetas <br> • Parrilla para barbacoa <br> • Toboganes <br>
+            • Mesas de pícnic <br> • Sanitario público <br> • Columpios</p>
+            <h3>Niños</h3>
+            <p>• Área de juegos <br> • Es recomendable para cumpleaños infantiles <br>
+            • Ideal para ir con niños <br> • Rutas de senderismo adecuadas para niños</p>
+         </div>
+      </section>
+      
+      <!--<h1 class="heading-title"> <br><br>Acerca del Parque Morelos </h1>
+      <section class="home3">
+         <div class="content3">
+            <h3>Accesibilidad</h3>
+            <p>• Entrada accesible para personas en silla de ruedas <br> 
+            • Estacionamiento accesible para personas en silla de ruedas</p>
+            <h3>Actividades</h3>
+            <p>• Senderismo <br> 
+            • Ciclismo</p>
+            <h3>Servicios</h3>
+            <p>• Área para patinetas <br> • Parrilla para barbacoa <br> • Toboganes <br>
+            • Mesas de pícnic <br> • Sanitario público <br> • Columpios</p>
+            <h3>Niños</h3>
+            <p>• Área de juegos <br> • Es recomendable para cumpleaños infantiles <br>
+            • Ideal para ir con niños <br> • Rutas de senderismo adecuadas para niños</p>
+         </div>
+      </section>-->
+
+      <div class="content2">
+            <h3>No olvides visitar nuestro Blog</h3>
+            <a href="/blog.php" class="btn">Blog de viajes</a>
+     </div>
+      <!-- termino sobre la sección de about -->
+
+      <!-- comienza la sección de pie de página  -->
+
+      <section class="footer">
+
+         <div class="box-container">
+
+            <div class="box">
+               <h3>Enlaces</h3>
+               <a href="home.php"> <i class="fas fa-angle-right"></i> Inicio</a>
+               <a href="directory.php"> <i class="fas fa-angle-right"></i> Directorio </a>
+               <a href="activities.php"> <i class="fas fa-angle-right"></i> Actividades </a>
+               <a href="blog.php"> <i class="fas fa-angle-right"></i> Blog de Viajes</a>
+            </div>
+
+            <div class="box">
+               <h3>Sobre Nosotros</h3>
+               <!--<a href="#"> <i class="fas fa-angle-right"></i> Preguntas</a>
+               <a href="#"> <i class="fas fa-angle-right"></i> Acerca de</a-->
+               <a href="#"> <i class="fas fa-angle-right"></i> Politica de privacidad</a>
+               <a href="#"> <i class="fas fa-angle-right"></i> Terminos de uso</a>
+            </div>
+
+            <div class="box">
+               <h3>Contactos</h3>
+               <a href="#"> <i class="fas fa-phone"></i> +52 (664) 214 99 81 </a>
+               <a href="#"> <i class="fas fa-phone"></i> +52 (664) 873 84 93 </a>
+               <a href="#"> <i class="fas fa-envelope"></i> omar.garcia201@tectijuana.edu.mx </a>
+               <a href="#"> <i class="fas fa-envelope"></i> jesus.saguilan201@tectijuana.edu.mx </a>
+               <a href="#"> <i class="fas fa-envelope"></i> maria.ramos201@tectijuana.edu.mx </a>
+            </div>
+
+            <div class="box">
+               <h3>Redes Sociales</h3>
+               <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+               <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+               <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+               <a href="#"> <i class="fab fa-linkedin"></i> Linkedin </a>
+            </div>
+
+         </div>
+
+         <div class="credit"> Creado por <span>Private Studio</span> | ¡Todos los derechos reservados! </div>
+
+      </section>
+
+      <!-- termina la sección de pie de página -->
+
+      <!-- enlace js deslizador  -->
+      <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+      <!-- enlace de archivo js personalizado  -->
+      <script src="/js/script.js"></script>
+
+   </body>
+</html>
